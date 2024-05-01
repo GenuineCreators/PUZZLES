96cbbad12ff323ccf5b9bb20a9bb00c342601a3f
GenuineCreators <benjamin.mbeva@strathmore.edu>
2024-05-01 08:17:20 +0300

    add services to database

--- lib/screens/service_screen.dart
+++ lib/screens/service_screen.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sellers/widgets/bottom_navbar.dart';
 import 'package:sellers/widgets/custom_button.dart';
 
@@ -14,10 +21,12 @@ class ServiceScreen extends StatefulWidget {
 
 class _ServiceScreenState extends State<ServiceScreen> {
   String? _selectedCategory;
-
   File? selectedImage;
-
-  List<Widget> _textFields = [];
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _smallVehiclePriceController = TextEditingController();
+  TextEditingController _mediumVehiclePriceController = TextEditingController();
+  TextEditingController _largeVehiclePriceController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
 
   void showImagePickerOption(BuildContext context) {
     showModalBottomSheet(
@@ -85,6 +94,60 @@ class _ServiceScreenState extends State<ServiceScreen> {
     });
   }
 
+  Future<String> _uploadImage(File imageFile) async {
+    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+        .ref()
+        .child('servicesimages/${DateTime.now().millisecondsSinceEpoch}');
+    await ref.putFile(imageFile);
+    String downloadURL = await ref.getDownloadURL();
+    return downloadURL;
+  }
+
+  Future<void> _addToDatabase(String imageUrl) async {
+    // Prepare data to store in Firebase Storage
+    Map<String, dynamic> data = {
+      'name': _nameController.text,
+      'package': _selectedCategory,
+      'small_vehicle_price': _smallVehiclePriceController.text,
+      'medium_vehicle_price': _mediumVehiclePriceController.text,
+      'large_vehicle_price': _largeVehiclePriceController.text,
+      'description': _descriptionController.text,
+      'image_url': imageUrl,
+    };
+
+    // Convert data to JSON string
+    String jsonData = jsonEncode(data);
+
+    // Create a Uint8List from the JSON string
+    Uint8List bytes = Uint8List.fromList(utf8.encode(jsonData));
+
+    // Upload data file to Firebase Storage
+    firebase_storage.Reference dataRef = firebase_storage
+        .FirebaseStorage.instance
+        .ref()
+        .child('services/${DateTime.now().millisecondsSinceEpoch}.json');
+
+    await dataRef.putData(bytes);
+
+    // Get download URL of the uploaded data file
+    String dataDownloadURL = await dataRef.getDownloadURL();
+
+    // Store data in Firestore
+    await FirebaseFirestore.instance.collection('services').add({
+      'name': _nameController.text,
+      'package': _selectedCategory,
+      'small_vehicle_price': _smallVehiclePriceController.text,
+      'medium_vehicle_price': _mediumVehiclePriceController.text,
+      'large_vehicle_price': _largeVehiclePriceController.text,
+      'description': _descriptionController.text,
+      'image_url': imageUrl,
+      'data_url': dataDownloadURL, // Store the download URL of the data file
+      'timestamp': DateTime.now(),
+    });
+
+    // Now, both the image and data are stored in Firebase Storage and Cloud Firestore
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -108,38 +171,21 @@ class _ServiceScreenState extends State<ServiceScreen> {
               child: GestureDetector(
                 onTap: () {
                   showImagePickerOption(context);
-                  // getImage(ImageSource.camera); // Open gallery to select image
                 },
                 child: Stack(
                   children: [
                     selectedImage != null
-                        ? Container(
+                        ? Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
                             width: double.infinity,
-                            height: 250,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.file(
-                                selectedImage!,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
+                            height: double.infinity,
                           )
-                        : Container(
+                        : Image.network(
+                            'https://images.unsplash.com/photo-1618641986557-1ecd230959aa?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            fit: BoxFit.cover,
                             width: double.infinity,
-                            height: 250,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.network(
-                                'https://images.unsplash.com/photo-1618641986557-1ecd230959aa?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
+                            height: double.infinity,
                           ),
                   ],
                 ),
@@ -156,6 +202,7 @@ class _ServiceScreenState extends State<ServiceScreen> {
             ),
             const SizedBox(height: 8),
             TextFormField(
+              controller: _nameController,
               decoration: InputDecoration(
                 labelText: 'Name ',
                 border: OutlineInputBorder(),
@@ -171,19 +218,6 @@ class _ServiceScreenState extends State<ServiceScreen> {
               onChanged: (newValue) {
                 setState(() {
                   _selectedCategory = newValue;
-                  _textFields.clear(); // Clear previous text fields
-                  // Add text fields based on selected category
-                  if (_selectedCategory != null) {
-                    if (_selectedCategory == 'Express') {
-                      _textFields.add(CarWashTextFields());
-                    } else if (_selectedCategory == 'Deluxe') {
-                      _textFields.add(CarWashTextFields());
-                    } else if (_selectedCategory == 'Ultimate') {
-                      _textFields.add(CarWashTextFields());
-                    } else {
-                      _textFields.add(GeneralTextFields());
-                    }
-                  }
                 });
               },
               items: [
@@ -199,123 +233,101 @@ class _ServiceScreenState extends State<ServiceScreen> {
                 );
               }).toList(),
             ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              controller: _smallVehiclePriceController,
+              decoration: InputDecoration(
+                labelText: "Small Vehicle Price",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              controller: _mediumVehiclePriceController,
+              decoration: InputDecoration(
+                labelText: "Medium Vehicle Price",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              controller: _largeVehiclePriceController,
+              decoration: InputDecoration(
+                labelText: "Large Vehicle Price",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                labelText: 'Description',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+            ),
             SizedBox(height: 20),
-            SizedBox(height: 20.0),
             SizedBox(
               width: double.infinity,
               child: CustomButton(
                 text: "Add",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BottomNavBar(),
-                    ),
-                  );
+                onPressed: () async {
+                  if (_selectedCategory == null ||
+                      _nameController.text.isEmpty ||
+                      _smallVehiclePriceController.text.isEmpty ||
+                      _mediumVehiclePriceController.text.isEmpty ||
+                      _largeVehiclePriceController.text.isEmpty ||
+                      _descriptionController.text.isEmpty ||
+                      selectedImage == null) {
+                    Fluttertoast.showToast(
+                      msg: "Please fill all fields and select an image",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                    );
+                    return;
+                  }
+                  try {
+
+                     // Show circular progress indicator
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      );
+                    String imageUrl = await _uploadImage(selectedImage!);
+                    await _addToDatabase(imageUrl);
+                    Fluttertoast.showToast(
+                      msg: "Product added successfully",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => BottomNavBar(),
+                      ),
+                    );
+                  } catch (error) {
+                    print("Error adding product: $error");
+                    Fluttertoast.showToast(
+                      msg: "Failed to add product",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                    );
+                  }
                 },
               ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class CarWashTextFields extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Enter price for each Car Type:'),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Small Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Medium Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Large Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: 'Description',
-            border: OutlineInputBorder(),
-          ),
-          maxLines: 3,
-        ),
-        SizedBox(height: 20.0),
-      ],
-    );
-  }
-}
-
-class GeneralTextFields extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Name Of Package",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.text,
-        ),
-        SizedBox(height: 20.0),
-        Text('Enter price for each Car Type:'),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Small Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Medium Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: "Large Vehicle Price",
-            border: OutlineInputBorder(),
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: 10.0),
-        TextFormField(
-          decoration: InputDecoration(
-            labelText: 'Description',
-            border: OutlineInputBorder(),
-          ),
-          maxLines: 3,
-        ),
-        SizedBox(height: 20.0),
-      ],
     );
   }
 }
